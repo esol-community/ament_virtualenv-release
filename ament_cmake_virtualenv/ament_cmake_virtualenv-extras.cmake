2adf94103cf8afdf558956d6105f7cbeca5b2393
Max Krichenbauer <mail@makx.net>
2019-12-04 17:18:34 +0900

    Trimmed the branch to only the ament_virtualenv sub directory

--- ament_cmake_virtualenv/ament_cmake_virtualenv-extras.cmake
+++
@@ -1,3 +0,0 @@
-# Include cmake modules from ament_cmake_virtualenv
-include(${ament_cmake_virtualenv_DIR}/ament_generate_virtualenv.cmake)
-include(${ament_cmake_virtualenv_DIR}/ament_install_python.cmake)
